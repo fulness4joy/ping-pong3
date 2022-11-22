5e36d12c9e465e50065c4cff288c4102b5225430
fulness4joy <117214478+fulness4joy@users.noreply.github.com>
2022-11-22 16:38:25 +0200

    Update ping_pong000.py
    Добавлен базовый класс для всех спрайтов и класс главного персонажа

--- ping_pong000.py
+++ ping_pong000.py
@@ -13,15 +13,47 @@ FPS = 60
 #Состояние игры
 GAME_STATE = True
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, image, width, height, x, y, speed):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(image), (width, height))
+        self.rect = self.image.get_rect()
+        self.speed = speed
+        self.rect.y = y
+        self.rect.x = x
+
+    def show(self):
+        WINDOW.blit(self.image, (self.rect))
+
+class Player(GameSprite):
+
+    def __init__(self, image, w,h, x,y, speed):
+        super().__init__(image, w,h, x,y, speed)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] == True:
+            self.rect.y -= 10
+        if keys[pygame.K_DOWN] == True:
+            self.rect.y += 10
+        
+
 WINDOW = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT), pygame.RESIZABLE)
 
 clock = pygame.time.Clock()
+
+rocket1 = Player(PATH + 'racket.png', 39, 136, 50, WIN_HEIGHT/2-68, 10)
 
 while GAME_STATE:
     events = pygame.event.get()
     for e in events:
         if e.type == pygame.QUIT:
             GAME_STATE = False
+
+    WINDOW.fill((200, 200, 0))
+
+    rocket1.move()
+    rocket1.show()
 
     clock.tick(FPS)
     pygame.display.update()
